54728dba07314030344ade3ccbd63f677884cd06
Tushar Sharma <97828116+Tushar1223@users.noreply.github.com>
2025-08-25 22:56:48 +0530

    Update index.js in frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-
 import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import axios from "axios";
 
 const options = {
   timeout: 5000,
@@ -13,11 +14,15 @@ const options = {
   transition: transitions.SCALE,
 };
 
-ReactDOM.render(
+// ✅ Global axios setup
+axios.defaults.baseURL = "https://eureka-ecommerceapp.onrender.com/api/v1"; 
+axios.defaults.withCredentials = true; // important for cookies/session
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
     <AlertProvider template={AlertTemplate} {...options}>
       <App />
     </AlertProvider>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
